3cbb31f3199a39998a95d9258a999a1dc9636520
Di_St <dima.stozhok335@gmail.com>
2025-07-09 23:47:56 +0300

    Basic CRUD operation for Task

---
+++ TaskManager.Application/DTO/TaskTimeCreateDto.cs
@@ -0,0 +1,8 @@
+namespace TaskManager.Application.DTO;
+
+public class TaskTimeCreateDto
+{
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string CategoryName { get; set; } = string.Empty;
+}
---
+++ TaskManager.Application/Services/TaskServices.cs
@@ -0,0 +1,39 @@
+using AutoMapper;
+using TaskManager.Application.DTO;
+using TaskManager.Application.Interfaces;
+using TaskManager.Domain.Interfaces;
+using TaskManager.Domain.Models;
+
+namespace TaskManager.Application.Services;
+
+public class TaskServices(IBaseRepository<TaskTime> taskRepository, IMapper mapper) : ITaskService
+{
+    public async Task<IEnumerable<TaskTime>> GetAllAsync() 
+        => await taskRepository.GetAllAsync();
+
+    public async Task<TaskTime> GetByIdAsync(int id) 
+        => await taskRepository.GetAsync(t => t.Id == id) ?? 
+           throw new Exception("Task not found");
+
+    public async Task CreateAsync(TaskTimeCreateDto createDto)
+    {
+        var result = await taskRepository.AddAsync(mapper.Map<TaskTime>(createDto));
+        if (! result)
+            throw new Exception("Task not found");
+    }
+
+    public async Task UpdateAsync(TaskTime taskTime)
+    {
+        var result = await taskRepository.UpdateAsync(mapper.Map<TaskTime>(taskTime));
+        if (!result)
+            throw new Exception("Task not found");
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var result = await taskRepository.DeleteAsync(
+            await taskRepository.GetAsync(t => t.Id == id));
+        if(!result)
+            throw new Exception("Task not found");
+    }
+}
