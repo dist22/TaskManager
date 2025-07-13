44561fb3e074644d81501daced04d329db3e7d71
Di_St <dima.stozhok335@gmail.com>
2025-07-14 01:06:55 +0300

    Created repo for UserTask

---
+++ TaskManager.Infrastructure/Repositories/UserTaskRepository.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Domain.Interfaces;
+using TaskManager.Domain.Models;
+using TaskManager.Infrastructure.Data.Context;
+
+namespace TaskManager.Infrastructure.Repositories;
+
+public class UserTaskRepository(DataContextEf entity) : BaseRepository<UserTask>(entity), IUserTaskRepository
+{
+    public async Task<IEnumerable<UserTask>> GetUsersByTaskIdAsync(int taskId)
+    {
+        return await _dbSet
+            .Include(ut => ut.User)
+            .Where(ut => ut.TaskId == taskId)
+            .ToListAsync();
+    }
+
+    public async Task<IEnumerable<UserTask>> GetTasksByUserIdAsync(int userId)
+    {
+        return await _dbSet
+            .Include(ut => ut.Task)
+            .Where(ut => ut.UserId == userId)
+            .ToListAsync();
+    }
+}
