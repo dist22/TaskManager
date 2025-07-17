8a283a0980c619d92cb1d4bce45232cf73765b8e
Di_St <dima.stozhok335@gmail.com>
2025-07-17 20:20:12 +0300

    Created filter for category

---
+++ TaskManager.Application/DTO/CategoryFilterDto.cs
@@ -0,0 +1,8 @@
+namespace TaskManager.Application.DTO;
+
+public class CategoryFilterDto
+{
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public bool? IsActive { get; set; }
+}
