0793a6ad0208f1cba3fc50fd734909671a4e3ba0
Di_St <dima.stozhok335@gmail.com>
2025-07-21 23:57:42 +0300

    Added logger

--- TaskManager.WebAPI/obj/Debug/net8.0/TaskManager.WebAPI.AssemblyInfo.cs
+++ TaskManager.WebAPI/obj/Debug/net8.0/TaskManager.WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+911a53d70b1564bda13e16065ffc12d333779446")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+841d85833c47b91e23d0691e5f431e67f80a2387")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
